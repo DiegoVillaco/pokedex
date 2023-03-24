7de85dda0b5751630be8ed8f96f71deaa8c06559
DiegoVillaco <Diego.Villarroel@jala.university>
2023-03-24 18:40:20 -0400

    update favorite button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import MediaCard from './components/MediaCard';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SignInComponent from './components/SingInComponent';
 import PageNotFoundComponent from './components/PageNotFoundComponent';
+import { ThemeProvider } from './context/ThemeContext';
+import BottomNav from './components/BottomNav';
+import NavBar from './components/NavBar';
+import StarIcon from '@mui/icons-material/Star';
+
+
+
 // import { Bar } from 'react-chartjs-2';
 
 
@@ -14,7 +21,10 @@ function App() {
   const [currentId, setCurrentId] = useState(1);
   const [pokemon, setPokemon] = useState({ sprites: {} });
   const [isLoading, setisLoading] = useState({ isLoading: false });
-  const [weakness,setWeakness]= useState([]);
+  const [weakness, setWeakness] = useState([]);
+  const [isFavorite, setFavorite]= useState(false);
+  const [myFavorites, setMyFavorites]= useState([])
+  
   useEffect(() => {
     setisLoading(true)
     fetch(`${pokeApiDomain}pokemon/${currentId}`)
@@ -26,91 +36,120 @@ function App() {
         console.log(pokemonData)
         //console.log(pokemonData);
         setisLoading(false);
-        
+
       })
-      
+
 
   }, [currentId]);
-  const pokemonWeakness=(types)=>{
-    
-    Promise.all(types.map((type)=>
+  const pokemonWeakness = (types) => {
+
+    Promise.all(types.map((type) =>
       fetch(`${pokeApiDomain}type/${type.type.name}/`)
-      .then(response => response.json())
-      .then(response => response.damage_relations.double_damage_from)
-    )).then((pokeweak)=>{
-        
-        let aux = []
-        pokeweak.forEach(weak => {
-          weak.forEach(element => {
-           
-            aux.push(element)
-            
-          });
-         
-        })
-        console.log(aux);
-        setWeakness(aux);
-        console.log('pokeweak',pokeweak)
+        .then(response => response.json())
+        .then(response => response.damage_relations.double_damage_from)
+    )).then((pokeweak) => {
+
+      let aux = []
+      pokeweak.forEach(weak => {
+        weak.forEach(element => {
+
+          aux.push(element)
+
+        });
 
       })
-      
-    
-    
+      console.log(aux);
+      setWeakness(aux);
+      console.log('pokeweak', pokeweak)
+
+    })
+
+
+
   }
-  
+
   const getPokemon = (id) => {
     setCurrentId(id);
   }
+
+  const addFavorite =(id)=>{
+    setMyFavorites([...myFavorites, id])
+    
+  }
+  const deleteFavorite =(id)=>{
+    const result = myFavorites.filter(notDeletedPokemon => notDeletedPokemon  !== id);
+
+    setMyFavorites(result);
+    
+  }
   
+  
+
   return (
     <div className="App">
+      <NavBar>
+
+      </NavBar>
       <header className="App-header">
         {
           isLoading ? (<></>)
             : (
 
               <div>
+                
                 <div>
+                  
 
                 </div>
-                
+
                 <div>
-                 <Routes>
-                  <Route path='/' element ={<SignInComponent/>}/>
-                  <Route path='/home' element={<MediaCard
-                  
-                    logo={pokemon.sprites.front_default} 
-                    image={pokemon.sprites.other['official-artwork'].front_default} 
-                    name={pokemon.name} 
-                    weight={pokemon.weight} 
-                    height={pokemon.height} 
-                    weakness={<div className='weakness-container'>
-                    {weakness?.map((wk,index)=>{
-                      return <span key={index}>{wk.name}</span>
-                    })}
-                </div>}
-                  />}></Route>
-                  <Route path='*' element = {<PageNotFoundComponent/>}/>
-                </Routes>
-                  
-                   
-                
+
+                  <ThemeProvider >
+                    <Routes>
+                      <Route path='/' element={<SignInComponent />} />
+                      <Route path='/home' element={<MediaCard
+                        id={pokemon.id}
+                        addFavorite={addFavorite}
+                        deleteFavorite={deleteFavorite}
+                        myFavorites={myFavorites}
+                        logo={pokemon.sprites.front_default}
+                        image={pokemon.sprites.other['official-artwork'].front_default}
+                        name={pokemon.name}
+                        weight={pokemon.weight}
+                        height={pokemon.height}
+                        weakness={<div className='weakness-container'>
+                        
+                          {weakness?.map((wk, index) => {
+                            return <span key={index}>{wk.name}</span>
+                          })}
+                        </div>}
+                      />}></Route>
+                      <Route path='*' element={<PageNotFoundComponent />} />
+                    </Routes>
+                    
+
+
+                  </ThemeProvider>
                 </div>
                 <div>
                   <button onClick={() => getPokemon(currentId - 1)}>Prev  </button>
                   <button onClick={() => getPokemon(currentId + 1)}>Next</button>
-                  
-                  
-                 
-                  
+
                 </div>
+                
 
               </div>
+              
             )
         }
+        
 
       </header>
-    </div>  
+      <BottomNav>
+                  
+        </BottomNav>
+    </div>
+    
   );
 }
 
--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,48 +1,70 @@
 import * as React from 'react';
-import { useState,useEffect } from 'react';
-import {Card,Avatar,IconButton, CardHeader, CardActions, CardContent, CardMedia, Button,Typography} from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Card, Avatar, IconButton, CardHeader, CardActions, CardContent, CardMedia, Button, Typography, Switch, createMuiTheme } from '@mui/material';
 //import {Bar } from 'react-chartjs-2';
-import {Chart} from './Chart.js';
+import { Chart } from './Chart.js';
+import ThemeContext from '../context/ThemeContext.js';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import StarIcon from '@mui/icons-material/Star';
+import { yellow } from '@mui/material/colors';
 
 
 
 
-export default function MediaCard({image, name, id, logo, weight,height,habili,weakness }) {
+
+export default function MediaCard({ addFavorite, deleteFavorite, myFavorites, image, name, id, logo, weight, height, habili, weakness, }) {
+  const data = React.useContext(ThemeContext);
+  console.log(myFavorites)
+  
   return (
-    <Card sx={{ maxWidth: 345, backgroundColor: 'green' }}>
-      <CardHeader 
-      avatar={
-        <Avatar sx= {{bgcolor:'white', boxShadow: 5, padding: '5px', margin:'2px' }} aria-label='recipe' src={logo}></Avatar>
-      }
-      titleTypographyProps={{variant:'h4', alig:'left'}}
-      title={name}
-      subheader={id}/>
-      <CardMedia
-        sx={{ 
+    <div className={data.theme}>
+
+      <Card sx={{ maxWidth: 345, backgroundColor: 'green' }}>
+
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: 'white', boxShadow: 5, padding: '5px', margin: '2px' }} aria-label='recipe' src={logo}></Avatar>
+          }
+          action={myFavorites.includes(id) ? 
+            <StarIcon sx={{ color: yellow[500] }} fontSize="large" onClick={() => deleteFavorite(id)} /> : 
+            <StarOutlineIcon fontSize="large" onClick={() => addFavorite(id)} />}
+          titleTypographyProps={{ variant: 'h4', alig: 'left' }}
+          title={name}
+          subheader={id} />
+        <CardMedia
+          sx={{
             minHeight: '300px',
             maxHeight: '300px',
-            padding:"15px",
-            objectFit:"contain",
-            backgroundColor : 'skyblue',
+            padding: "15px",
+            objectFit: "contain",
+            backgroundColor: 'skyblue',
             width: "auto"
 
-     }}
-        image={image}
-        title={name}
-      />
-      <CardContent>
-        
-        <Typography variant="body2" color="text.secondary" weight={weight} height={height}>
-          Weight {weight} lbs, Height {height} 
-        </Typography>
-        <Typography sx={{padding:'5px'}} variant="body2" color="text.secondary"  >
-          Weakness {weakness}
-        </Typography>
-        {/* <Bar width="200" height="200" data={data} options={options} /> */}
-        <Chart></Chart>
-        
-      </CardContent>
-     
-    </Card>
+          }}
+          image={image}
+          title={name}
+        />
+        <CardContent>
+
+          <Typography variant="body2" color="text.secondary" weight={weight} height={height}>
+            Weight {weight} lbs, Height {height}
+          </Typography>
+          <Typography sx={{ padding: '5px' }} variant="body2" color="text.secondary"  >
+            Weakness {weakness}
+          </Typography>
+          {/* <Bar width="200" height="200" data={data} options={options} /> */}
+          <Chart></Chart>
+
+        </CardContent>
+
+      </Card>
+      <label htmlFor='light'>Light</label>
+      <Switch name="theme" onChange={data.handleTheme} value={data.theme} />
+      <label htmlFor='dark'>Dark</label>
+
+
+
+
+    </div>
   );
 }
